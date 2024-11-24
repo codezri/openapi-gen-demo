7b04154efe6c435b36d0f376f8b49ede300f132b
Shalitha Suranga <shalithasuranga@gmail.com>
2024-11-24 13:00:06 +0530

    Complete app functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductService } from './services/product.service';
-import { DefaultService, Product } from './modules/openapi';
+import { ProductService, Product } from './modules/openapi';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,11 +16,29 @@ import { AsyncPipe } from '@angular/common';
 export class AppComponent {
   title = 'openapi-demo';
   products$!: Observable<Product[]>;
+  productName!: string;
 
-  constructor(private productService: DefaultService) {
+  constructor(private productService: ProductService) {
   }
 
   ngOnInit() {
-    this.products$ = this.productService.productsGet();
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.products$ = this.productService.getProducts();
+  }
+
+  deleteProduct(productId: number) {
+    this.productService.deleteProduct(productId)
+      .subscribe(() => this.loadProducts())
+  }
+
+  createProduct() {
+    this.productService.createProduct({name: this.productName})
+      .subscribe(() =>  {
+        this.productName = '';
+        this.loadProducts();
+      })
   }
 }
--- server/main.js
+++ server/main.js
@@ -22,14 +22,13 @@ const options = {
 
 const apiSpec = swaggerJsdoc(options);
 fs.writeFileSync('openapi.yaml', apiSpec);
+console.log('Wrote OpenAPI spec to openapi.yaml');
 
 app.use(express.json());
 app.use(cors());
 
 
-let products = [
-    { id: 1, name: 'Book' }
-];
+let products = [];
 
 /**
  * @openapi
@@ -40,22 +39,25 @@ let products = [
  *       properties:
  *         id:
  *           type: integer
- *           description: Product identifier.
- *           example: 1
  *         name:
  *           type: string
- *           description: Product name.
- *           example: MacBook Pro
+*     Error:
+ *       type: object
+ *       properties:
+ *         error:
+ *           type: string
  */
 
 /**
  * @openapi
  * /products:
  *   get:
- *     description: Lists all products
+ *     operationId: getProducts
+ *     tags: 
+ *       - product
  *     responses:
  *       200:
- *         description: OK.
+ *         description: OK
  *         content:
  *           application/json:
  *             schema:
@@ -67,32 +69,109 @@ app.get('/products', (req, res) => {
     res.json(products);
 });
 
+/** 
+ * @openapi
+ * /products:
+ *   post:
+ *     operationId: createProduct
+ *     tags: 
+ *       - product
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *             required:
+ *              - name
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product' 
+ *       400:
+ *         description: Invalid data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error' 
+*/
 app.post('/products', (req, res) => {
     const { name } = req.body;
-    console.log(name);
-    const latestProduct = products.reduce((prev, cur) => cur.id > prev.id ? cur : prev);
-    const id = latestProduct ? latestProduct.id + 1 : 1;
-    products.push({ id, name });
-    res.json(products);
+    if (!name) {
+        return res.status(400).json({ error: 'Missing product name' });
+    }
+
+    const latestProduct = products.reduce((prev, cur) => cur.id > prev.id ? cur : prev, {id: 0});
+    const product = { id: latestProduct.id + 1, name };
+
+    products.push(product);
+    res.status(201).json(product);
 });
 
+/**
+ * @openapi
+ * /products/{productId}:
+ *   get:
+ *     operationId: getProduct
+ *     tags: 
+ *       - product
+ *     parameters:
+ *       - name: productId
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200: 
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                 $ref: '#/components/schemas/Product'
+ */
 app.get('/products/:productId', (req, res) => {
     const product = products.find((product) => product.id === parseInt(req.params.productId));
+
     if (!product) {
         return res.status(404).json({ error: 'Product not found' });
     }
     res.json(product);
 });
 
+/**
+ * @openapi
+ * /products/{productId}:
+ *   delete:
+ *     operationId: deleteProduct
+ *     tags: 
+ *       - product
+ *     parameters:
+ *       - name: productId
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: Deleted
+ *       404:
+ *         description: Not found
+ */
 app.delete('/products/:productId', (req, res) => {
     const productId = products.findIndex((product) => product.id === parseInt(req.params.productId));
+
     if (productId == -1) {
         return res.status(404).json({ error: 'Product not found' });
     }
-    products.splice(productId);
+    products.splice(productId, 1);
     res.end();
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`OpenAPI demo server listening on port ${port}`)
 })
